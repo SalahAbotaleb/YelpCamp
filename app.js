80270a181550d2cf0d446d5ff0b2df76c29cc652
salah <salah.m.abotaleb@gmail.com>
2023-09-10 19:25:31 +0300

    editing ExpressError path

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const engine = require("ejs-mate");
 
-const ExpressError = require("./utils/expressError.js");
+const { ExpressError } = require("./utils/expressError");
 
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -5,4 +5,4 @@ class ExpressError extends Error {
         this.status = status;
     }
 }
-module.exports = ExpressError;
+module.exports.ExpressError = ExpressError;
